dd8a9b7979ab6478d3592fa7021e1fc65d7637ca
spykins <waleola.akinsanmi@gmail.com>
2018-09-02 23:55:30 -0400

    close the navigation bar when any item in the bar is clicked

--- src/screens/navigation/SideDrawer.js
+++ src/screens/navigation/SideDrawer.js
@@ -1,32 +1,51 @@
-import React, {Component} from 'react';
-import {View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 
 
 class SideDrawer extends Component {
+
+    handleFeedNavButton = () => {
+        this.closeNavigation();
+    }
+
+    handleContinentNavButton = () => {
+        this.closeNavigation();
+    }
+
+    handleFavouriteNavButton = () => {
+        this.closeNavigation();
+    }
+
+    closeNavigation = () => {
+        this.props.navigator.toggleDrawer({
+            side: "left"
+        });
+    }
+
     render() {
         return (
-            <View  style={[styles.container, {width: Dimensions.get("window").width * 0.8}]}>
-                <TouchableOpacity>
+            <View style={[styles.container, { width: Dimensions.get("window").width * 0.8 }]}>
+                <TouchableOpacity onPress={this.handleFeedNavButton}>
                     <View style={styles.drawerItem}>
-                        <Icon name="logo-rss" size={30} color="#aaa"  style={styles.drawerItemIcon}/>
+                        <Icon name="logo-rss" size={30} color="#aaa" style={styles.drawerItemIcon} />
                         <Text>Feeds</Text>
                     </View>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
-                <View style={styles.drawerItem}>
-                    <Icon name={Platform.OS === 'android' ? "md-flag" : "ios-flag"} size={30} color="#aaa"  style={styles.drawerItemIcon}/>
-                    <Text>Continent</Text>
-                </View>
-            </TouchableOpacity>
+                <TouchableOpacity onPress={this.handleContinentNavButton}>
+                    <View style={styles.drawerItem}>
+                        <Icon name={Platform.OS === 'android' ? "md-flag" : "ios-flag"} size={30} color="#aaa" style={styles.drawerItemIcon} />
+                        <Text>Continent</Text>
+                    </View>
+                </TouchableOpacity>
 
-            <TouchableOpacity>
-                <View style={styles.drawerItem}>
-                    <Icon name={Platform.OS === 'android' ? "md-bookmark" : "ios-bookmark"} size={30} color="#aaa"  style={styles.drawerItemIcon}/>
-                    <Text>Favourite</Text>
-                </View>
-            </TouchableOpacity>
+                <TouchableOpacity onPress={this.handleFavouriteNavButton}>
+                    <View style={styles.drawerItem}>
+                        <Icon name={Platform.OS === 'android' ? "md-bookmark" : "ios-bookmark"} size={30} color="#aaa" style={styles.drawerItemIcon} />
+                        <Text>Favourite</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
 
         );
@@ -49,8 +68,6 @@ const styles = StyleSheet.create({
     drawerItemIcon: {
         marginRight: 10,
     }
-
-
 });
 
 export default SideDrawer;
